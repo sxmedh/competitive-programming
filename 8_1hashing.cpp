2e84770d9e37b275fcd9ea8353a1d424aefb51f9
sxmedh <sumedh11.sg@gmail.com>
2023-07-15 01:06:06 +0530

    commit

--- 8_1hashing.cpp
+++ 8_1hashing.cpp
@@ -6,7 +6,9 @@ void Frequency(int arr[], int n)
     unordered_map<int, int> map;
 
     for (int i = 0; i < n; i++)
+    {
         map[arr[i]]++;
+    }
 
     int maxFreq = 0, minFreq = n;
     int maxEle = 0, minEle = 0;
